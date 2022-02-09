7a4b7ae74ab5ce4095c218161bbb39538f5485bf
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-02-09 17:09:26 +0000

    Initial working recording log

--- code/record_screen.py
+++ code/record_screen.py
@@ -1,4 +1,10 @@
-from talon import Module, Context, actions, app, ui
+import json
+from pathlib import Path
+import subprocess
+import time
+from typing import Any
+import uuid
+from talon import Module, Context, actions, app, ui, speech_system, scope
 
 mod = Module()
 mod.tag(
@@ -13,6 +19,11 @@ mode: sleep
 and tag: user.recording_screen
 """
 
+recording_screen_ctx = Context()
+recording_screen_ctx.matches = r"""
+tag: user.recording_screen
+"""
+
 
 def show_obs_menu():
     menu = ui.apps(bundle="com.obsproject.obs-studio")[0].children.find_one(
@@ -25,11 +36,25 @@ def show_obs_menu():
     return menu
 
 
+recording_start_time = 0
+recording_log_directory = ""
+recording_log_file = ""
+recordings_root_dir = Path.home() / "talon-recording-logs"
+
+
+def log_object(output_object):
+    with open(recording_log_file, "a") as out:
+        out.write(json.dumps(output_object) + "\n")
+
+
 @mod.action_class
 class Actions:
     def record_screen_start():
         """Start recording screen"""
-        
+        global recording_start_time
+        global recording_log_directory
+        global recording_log_file
+
         # First ensure that OBS is running
         try:
             next(app for app in ui.apps(background=False) if app.name == "OBS")
@@ -51,10 +76,58 @@ class Actions:
             "AXPress"
         )
 
-        # Start quick time screen recording
+        # Start quicktime screen recording
         actions.key("cmd-shift-5")
         actions.sleep("500ms")
         actions.key("enter")
+
+        recording_start_time = time.perf_counter()
+        recording_log_directory = recordings_root_dir / time.strftime("%Y%m%dT%H%M%S")
+        recording_log_directory.mkdir(parents=True)
+        recording_log_file = recording_log_directory / "talon-log.jsonl"
+
+        # TODO: Enable silent cursorless record mode
+
+        user_dir: Path = actions.path.talon_user()
+        for directory in user_dir.iterdir():
+            if not directory.is_dir():
+                continue
+
+            repo_url = subprocess.run(
+                [
+                    "/usr/local/bin/gh",
+                    "repo",
+                    "view",
+                    "-b",
+                    "main",
+                    "--json",
+                    "url",
+                    "-q",
+                    ".url",
+                ],
+                capture_output=True,
+                text=True,
+                cwd=directory,
+            ).stdout.strip()
+
+            if not repo_url:
+                continue
+
+            commit_sha = subprocess.run(
+                ["/usr/local/bin/git", "rev-parse", "HEAD"],
+                capture_output=True,
+                text=True,
+                cwd=directory,
+            ).stdout.strip()
+
+            log_object(
+                {
+                    "type": "directoryInfo",
+                    "localPath": str(directory),
+                    "repoUrl": repo_url,
+                    "commitSha": commit_sha,
+                }
+            )
 
         actions.user.sleep_all()
 
@@ -73,14 +146,67 @@ class Actions:
         menu.children.find_one(AXRole="AXMenuItem", AXTitle="Stop Recording").perform(
             "AXPress"
         )
-        
+
         # Stop quick time screen recording
         ui.apps(bundle="com.apple.screencaptureui")[0].children.find_one(
             AXRole="AXMenuBarItem"
         ).perform("AXPress")
 
+        # TODO: Disable silent cursorless record mode
+
+    def maybe_capture_phrase(j: Any):
+        """Possibly capture a phrase; does nothing unless screen recording is active"""
+        pass
+
 
 @sleeping_recording_screen_ctx.action_class("user")
-class UserActions:
+class SleepUserActions:
     def maybe_show_history():
         pass
+
+
+@ctx.action_class("user")
+class UserActions:
+    def maybe_capture_phrase(j: Any):
+        # Turn this one off globally
+        pass
+
+
+@recording_screen_ctx.action_class("user")
+class UserActions:
+    def maybe_capture_phrase(j: Any):
+        global recording_start_time
+        words = j.get("text")
+
+        if text := actions.user.history_transform_phrase_text(words):
+            # TODO: Parse sim output and dump to file
+            # TODO: Get github link to talon file, making sure to follow symlinks
+            # QUESTION: Should we spy actions?
+
+            sim = None
+            commands = None
+            try:
+                sim = speech_system._sim(text)
+                commands = actions.user.parse_sim(sim)
+            except Exception as e:
+                app.notify(f'Couldn\'t sim for "{text}"', f"{e}")
+
+            log_object(
+                {
+                    "type": "talonCommandPhrase",
+                    "id": str(uuid.uuid4()),
+                    "timeOffset": time.perf_counter() - recording_start_time,
+                    "phrase": text,
+                    "rawSim": sim,
+                    "commands": commands,
+                    "modes": list(scope.get("mode")),
+                    "tags": list(scope.get("tag")),
+                }
+            )
+
+
+def on_phrase(j):
+    actions.user.maybe_capture_phrase(j)
+
+
+speech_system.register("phrase", on_phrase)
--- custom/code/history.py
+++ custom/code/history.py
@@ -1,12 +1,13 @@
 from typing import Optional
-from talon import Context
+from talon import Context, actions
 
 ctx = Context()
+
 
 @ctx.action_class("user")
 class UserActions:
     def history_transform_phrase_text(words: list[str]) -> Optional[str]:
-        if not words:
+        if not words or not actions.speech.enabled():
             return None
 
         try:
