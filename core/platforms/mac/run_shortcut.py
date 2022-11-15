e9055816848db9b39d38731478af089a039ae76a
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-11-15 16:28:11 +0000

    Add async shortcut

--- core/platforms/mac/run_shortcut.py
+++ core/platforms/mac/run_shortcut.py
@@ -14,8 +14,14 @@ class Actions:
     def run_shortcut(name: str):
         """Runs a shortcut on macOS"""
 
+    def run_shortcut_async(name: str):
+        """Runs a shortcut on macOS, and don't wait for it to finish"""
+
 
 @ctx.action_class("user")
 class UserActions:
     def run_shortcut(name: str):
         appscript.app(id="com.apple.shortcuts.events").shortcuts[name].run_()
+
+    def run_shortcut_async(name: str):
+        actions.user.system_command_nb(f'/usr/bin/shortcuts run "{name}"')
