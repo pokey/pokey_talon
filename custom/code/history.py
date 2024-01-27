9f5ff56d7e98c9ba8efe31904f1117aa98fa6237
Pokey Rule <755842+pokey@users.noreply.github.com>
2024-01-27 14:16:17 +0000

    don't collect history during test

--- custom/code/history.py
+++ custom/code/history.py
@@ -3,6 +3,9 @@ from typing import Optional
 from talon import Context, actions
 
 ctx = Context()
+ctx.matches = """
+not mode: user.cursorless_spoken_form_test
+"""
 
 
 @ctx.action_class("user")
@@ -17,3 +20,15 @@ class UserActions:
             pass
 
         return " ".join(words) if words else None
+
+
+cursorless_spoken_form_test_ctx = Context()
+cursorless_spoken_form_test_ctx.matches = """
+mode: user.cursorless_spoken_form_test
+"""
+
+
+@cursorless_spoken_form_test_ctx.action_class("user")
+class UserActions:
+    def history_transform_phrase_text(words: list[str]) -> Optional[str]:
+        return None
